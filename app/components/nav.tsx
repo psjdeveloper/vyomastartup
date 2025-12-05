1f3767f4c9d2c167b4ae77454ae28ec6067c05a2
psjdeveloper <prasoonsinghjadon.outlook.com>
2025-12-05 20:12:45 +0530

    Add navigation links for Books, Magazine, Toolkit, and Android app; fix section layout in Home page

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -24,6 +24,18 @@ export default function Nav(){
         <a href="https://vyomaco.substack.com/" className="text-white font-semibold hover:text-gray-200">
             newslette
         </a>
+        <a href="https://vyoma-books.vercel.app/" className="text-white font-semibold hover:text-gray-200">
+            Books
+        </a>
+         <a href="https://vyomaco.wordpress.com/?_gl=1*sh83pk*_gcl_au*MTI4MTE4MTQ1Ny4xNzY0NzYyNDMyLjE2NTUwNjM0MDIuMTc2NDg1NTgwNC4xNzY0ODU2MDYx" className="text-white font-semibold hover:text-gray-200">
+            Magzine
+        </a>
+            <a href="https://vyoma-os.vercel.app/" className="text-white font-semibold hover:text-gray-200">
+            All in one toolkit
+        </a>
+         <a href="https://github.com/psjdeveloper/vyomaco/releases/tag/vyoma" className="text-white font-semibold hover:text-gray-200">
+            Android app
+        </a>
         </li>
     </ul>           
           </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -80,6 +80,7 @@ export default function Home() {
           
           </div>
         </div>
+      
       </section>
     </>
   );
